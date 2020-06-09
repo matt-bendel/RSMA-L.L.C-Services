6b165e1a3e43a203f84597a5a3b288d5121bebfd
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2020-06-08 21:10:03 -0400

    Temporarily commented out email logic

--- contact/index.php
+++ contact/index.php
@@ -46,37 +46,27 @@ if (!isset($_POST['contact'])) {
     } else {
         $subject = $name . ' - ' . $email . ' - ' . $county . ' County';
 
-        if (!isset($GLOBALS['sent'])) {
-            $to = 'mattbendel60@gmail.com';
-
+//        if (!isset($GLOBALS['sent'])) {
+//            $to = 'mattbendel60@gmail.com';
+//
 //            $email = new \SendGrid\Mail\Mail();
-//            $email->setFrom("auto@rsmallcservices.com", "Auto");
+//            $email->setFrom("noreply@rsmallcservices.com");
 //            $email->setSubject($subject);
-//            $email->addTo($to, "Example User");
+//            $email->addTo($to);
 //            $email->addContent("text/plain", $message);
-//            $email->addContent(
-//                "text/html", "<strong>$message</strong>"
-//            );
-            $email = new \SendGrid\Mail\Mail();
-            $email->setFrom("test@example.com", "Example User");
-            $email->setSubject("Sending with SendGrid is Fun");
-            $email->addTo("test@example.com", "Example User");
-            $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-            $email->addContent(
-                "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-            );
-            $sendgrid = new \SendGrid('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');
-            try {
-                $response = $sendgrid->send($email);
-                print $response->statusCode() . "\n";
-                print_r($response->headers());
-                print $response->body() . "\n";
-            } catch (Exception $e) {
-                die('Caught exception: '. $e->getMessage() ."\n");
-            }
-
-            $GLOBALS['sent'] = 'sent';
-        }
+//
+//            $sendgrid = new \SendGrid('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');
+//            try {
+//                $response = $sendgrid->send($email);
+//                print $response->statusCode() . "\n";
+//                print_r($response->headers());
+//                print $response->body() . "\n";
+//            } catch (Exception $e) {
+//                die('Caught exception: '. $e->getMessage() ."\n");
+//            }
+//
+//            $GLOBALS['sent'] = 'sent';
+//        }
 
         getPostContactView($subject, $message);
     }
